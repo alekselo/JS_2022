aca06b3613d54aabfcd6250cd12124192f2f5aee
Alexey Loktev <a.a.loktev@yandex.ru>
2022-05-29 21:17:04 +0300

    Навёл порядок в коде

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 "use strict";
 
-let title = "Название проекта";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 5000;
-let rollback = Math.round(Math.random() * 101);
-let fullPrice = 30000;
-let adaptive = true;
+const title = "Верстка сайтов";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 5000;
+const rollback = Math.round(Math.random() * 101);
+const fullPrice = 30000;
+const adaptive = true;
 
 console.log(typeof title);
 console.log(typeof fullPrice);
@@ -13,8 +13,7 @@ console.log(typeof adaptive);
 console.log(screens.length);
 console.log("Стоимость верстки экранов: " + screenPrice + " рублей.");
 console.log("Стоимость разработки сайта: " + fullPrice + " рублей.");
-screens = screens.toLowerCase().split();
-console.log(screens);
+console.log(screens.toLowerCase().split());
 console.log(
   "Процент отката посреднику за работу: " +
     fullPrice * (rollback / 100) +
