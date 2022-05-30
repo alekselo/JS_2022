44e00b58a58f0a6e2ed677ade83ed53f7562e8f7
Alexey Loktev <a.a.loktev@yandex.ru>
2022-05-30 11:00:54 +0300

    Выполнено задание урока №3

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 "use strict";
 
-const title = "Верстка сайтов";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 5000;
-const rollback = Math.round(Math.random() * 101);
-const fullPrice = 30000;
-const adaptive = true;
+const title = prompt("Как называется Ваш проект?");
+const screens = prompt(
+  "Какие типы экранов нужно разработать?",
+  "Простые, Сложные, Интерактивные"
+);
+const screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой еще дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?");
+const rollback = 15;
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+
+switch (true) {
+  case fullPrice >= 30000:
+    console.log("Даем скидку в 10%");
+    break;
+  case fullPrice >= 15000 && fullPrice < 30000:
+    console.log("Даем скидку в 5%");
+    break;
+  case fullPrice < 15000 && fullPrice >= 0:
+    console.log("Скидка не предусмотрена");
+    break;
+  case fullPrice < 0:
+    console.log("Что то пошло не так");
+    break;
+}
 
 console.log(typeof title);
 console.log(typeof fullPrice);
@@ -19,3 +42,4 @@ console.log(
     fullPrice * (rollback / 100) +
     " рублей."
 );
+console.log(servicePercentPrice);
